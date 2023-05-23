3c3524f6c32f8c8ff1e4cf11654b40bc0ed3072a
Jack Hsu <jack.hsu@gmail.com>
2023-05-23 14:13:46 -0400

    create separate build target that generates schems that should be run before app run

--- packages/bar/src/main.ts
+++ packages/bar/src/main.ts
@@ -1,8 +1,9 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, mkdirSync } from 'fs';
 import * as process from 'process';
 
 console.log('generating bar schema at generated/bar/api.txt');
 
-writeFileSync('generated/bar/api.txt', `I am ${process.env.APP_NAME}`, {encoding: 'utf-8'})
+mkdirSync('generated/bar', { recursive: true });
+writeFileSync('generated/bar/api.txt', `5 I am ${process.env.APP_NAME}`, { encoding: 'utf-8' })
 
 process.exit();
